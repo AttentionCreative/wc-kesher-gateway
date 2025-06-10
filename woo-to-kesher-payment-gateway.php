cf64f813603da2e348a52f5821aaf2b3b29b464f
AttentionIdanHadad <139991695+AttentionIdanHadad@users.noreply.github.com>
2025-06-10 13:35:00 +0300

    Update woo-to-kesher-payment-gateway.php

--- woo-to-kesher-payment-gateway.php
+++ woo-to-kesher-payment-gateway.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/AttentionCreative/wc-kesher-gateway
  * Update URI: https://github.com/AttentionCreative/wc-kesher-gateway
  * Description: תוסף זה נועד לשלב תשלום קשר ב-WooCommerce.
- * Version: 1.0.5
+ * Version: 1.0.6
  * Requires at least: 5.2
  * Requires PHP: 7.2
  * Author: Attention Creative
